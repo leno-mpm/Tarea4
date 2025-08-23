eadbf3dc185485f919b25e02b530e1afaabf4d7d
FrancisLoayza <faloayza@espol.edu.ec>
2025-08-23 14:54:17 -0500

    Pull Up Method
    Se elimino sobrescritura de las classes ExtursionTerrestre, PaqueteBebidas, y TratamientoSpa.
    Se sobrescribieron los metodos en la clase padre

--- cruisebook/src/main/java/ec/edu/espol/ReservaDecorator.java
+++ cruisebook/src/main/java/ec/edu/espol/ReservaDecorator.java
@@ -1,5 +1,7 @@
 package ec.edu.espol;
 
+import java.util.Date;
+
 public abstract class ReservaDecorator implements Reserva {
     protected Reserva reserva;
 
@@ -27,5 +29,59 @@ public abstract class ReservaDecorator implements Reserva {
 
     public void pendiente(){
         reserva.pendiente();
+    }
+
+     @Override
+    public EstadoReserva getEstado() {
+        return reserva.getEstado();
+    }
+     @Override
+    public Crucero getCrucero() {
+        return reserva.getCrucero();
+    }
+
+    @Override
+    public PoliticaCancelacion getPoliticaCancelacion() {
+        return reserva.getPoliticaCancelacion();
+    }
+    
+    @Override
+    public void setEstado(EstadoReserva estado) {
+        reserva.setEstado(estado);
+    }
+
+    @Override
+    public Cabina getCabina() {
+        return reserva.getCabina(); 
+    }
+
+    @Override
+    public void setCabina(Cabina cabina) {
+       reserva.setCabina(cabina);
+    }
+
+    @Override
+    public Usuario getUsuario() {
+        return reserva.getUsuario();
+    }
+
+    @Override
+    public ViajeCrucero getViajeCrucero() {
+        return reserva.getViajeCrucero();
+    }
+
+    @Override
+    public int getId() {
+        return reserva.getId();
+    }
+
+    @Override
+    public void setFechaReserva(Date fechaReserva) {
+        reserva.setFechaReserva(fechaReserva);
+    }
+
+    @Override
+    public void setViajeCrucero(ViajeCrucero viajeCrucero) {
+        reserva.setViajeCrucero(viajeCrucero);
     }
 }
